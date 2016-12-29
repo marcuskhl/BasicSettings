a59d05b0c0d5d5a75d256ed5885f3ad0b982e3d6
Marcus <marcus0308@live.hk>
2016-12-29 23:10:05 +0000

    sa

--- R/zzz.R
+++ R/zzz.R
@@ -1,18 +1,18 @@
 .onLoad <- function(libname, pkgname){
   list.of.packages <- c("XML", "rvest", "Hmisc", "MASS","RevoUtilsMath", "RevoUtils","sparkTable",
                         "rio", "psych","readr","data.table","magrittr","sparklyr",
-                        "shiny","flexdashboard","gmodels","janitor", 
+                        "shiny","flexdashboard","gmodels","janitor",
                         "foreach","profvis","sqldf","reshape2","dplyr","zoo",
                         "scales","RODBC", "readxl","openxlsx","doParallel","dplyr","tidyr")
   new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-  suppressMessages(update.packages(installed.packages()[,"Package"]))
-  if(length(new.packages)) suppressMessages(install.packages(new.packages))
-  suppressMessages(lapply(list.of.packages, library, character.only = TRUE))
-  
+  update.packages(installed.packages()[,"Package"])
+  if(length(new.packages)) install.packages(new.packages)
+  lapply(list.of.packages, library, character.only = TRUE)
+
   cl <- makeCluster(4)
   registerDoParallel(cl)
-  
+
   cat("\014")
   print("Libraries Loaded")
-  print("Custom Functions Loaded")
+
 }
