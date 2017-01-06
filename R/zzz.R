685807d2771bcd6bbbe5d172410d031153dc7689
Leung <Marcus.Leung@ihs.com>
2017-01-06 15:38:21 +0000

    startup msg

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,12 @@
   registerDoParallel(cl)
   options(digits=15)
   cat("\014")
-  print("Libraries Loaded")
-  print("Functions Loaded")
+  # print("Libraries Loaded")
+  # print("Functions Loaded")
+}
+
+
+.onAttach <- function(...) {
+  packageStartupMessage("Libraries Loaded\n")
+  packageStartupMessage("Functions Loaded\n")
 }
