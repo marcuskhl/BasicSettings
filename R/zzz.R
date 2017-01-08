cb462708461521d8ecd48773aaa8cdd642f8b556
Marcus <marcus0308@live.hk>
2017-01-08 12:42:32 +0000

    123

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,9 @@
 .onLoad <- function(libname, pkgname){
   packages.installer()
-  cl <- makeCluster(2)
+  cl <- makeCluster(4)
   registerDoParallel(cl)
   options(digits=15)
   cat("\014")
-  # packageStartupMessage("Libraries Loaded\n")
-  # packageStartupMessage("Functions Loaded\n")
-
-
-  # print("Libraries Loaded")
-  # print("Functions Loaded")
 }
 
 
