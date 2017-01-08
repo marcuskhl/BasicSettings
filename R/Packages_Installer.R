a228bbc276dc5f5ef309be0a5d2c364e559a2800
Marcus <marcus0308@live.hk>
2017-01-08 01:38:14 +0000

    sdf

--- R/Packages_Installer.R
+++ R/Packages_Installer.R
@@ -1,30 +1,47 @@
 #' installs necessary packages
 packages.installer <- function(){
-  list.of.packages <- c("XML", "rvest", "Hmisc", "MASS",
-                        "sparkTable","jsonlite","rattle",
-                        "rio", "psych","readr","data.table","magrittr","sparklyr", "Rfacebook","RColorBrewer","RCurl","httpuv","httr","rjson",
-                        "shiny","flexdashboard","gmodels","janitor", "caret","ROCR",
-                        "foreach","profvis","sqldf","reshape2","dplyr","zoo",
-                        "scales","RODBC", "readxl","openxlsx","doParallel","dplyr","tidyr")
-  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-  update.packages(installed.packages()[,"Package"])
-  if(length(new.packages)) install.packages(new.packages)
-  lapply(list.of.packages, library, character.only = TRUE)
+  list.of.packages <- c(
+    # Web Scraping Tools:
+    "XML", "rvest", "jsonlite", "rjson",
+    # Misc Tools:
+    "Hmisc", "MASS",
+    # Big Data Stuff:
+    "sparkTable", "sparklyr", "Rfacebook", "twitteR", "RCurl", "httpuv", "httr",
+    # Rattle:
+    "RGtk2","rattle",# problematic packages
+    # Visualisation Tools:
+    "shiny","flexdashboard",
+    # Data Manipulation Tools:
+    "reshape2","dplyr","dplyr", "dtplyr", "tidyr", "janitor", "data.table","magrittr",# does not have reshape1
+    # Modelling Tools:
+    "psych", "gmodels", "caret", "ROCR",
+    # Multithreading:
+    "foreach", "doParallel",
+    # Input-Output:
+    "sqldf", "RODBC", "readxl","openxlsx", "rio", "readr",
+    # Misc:
+    "zoo", "profvis", "scales", "RColorBrewer"
+  )
 
-  cl <- makeCluster(4)
+  update_and_install <- function(list.of.packages){
+    new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+    update.packages(installed.packages()[,"Package"])
+    if(length(new.packages)) install.packages(new.packages,depen=T)
+    lapply(list.of.packages, library, character.only = TRUE)
+  }
+  update_and_install(list.of.packages)
+
+
+  cl <- makeCluster(4)  # might have to change it back to 2 for crappy laptops
   registerDoParallel(cl)
 
 
+  # For hardcore performance entheusiasts
   if(!is.na( match("Revo.version", ls(.BaseNamespaceEnv)))){
     list.of.packages <- c("RevoUtilsMath", "RevoUtils")
-    new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-    update.packages(installed.packages()[,"Package"])
-    if(length(new.packages)) install.packages(new.packages)
-    lapply(list.of.packages, require, character.only = TRUE)
+    update_and_install(list.of.packages)
     setMKLthreads(4)
   }
-
-
 
   cat("\014")
   print("Libraries Loaded")
